cacfa3aac3e817313edbde2bb81ccf602e25f51d
cpluis <36276548+cpluis@users.noreply.github.com>
2021-07-27 04:00:09 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/pages/MovieDetails/index.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Card from "../../components/Card";
+import GetDetailsMovie from "../../service/Movie/GetDetailsMovie";
+
+
+// import { Container } from './styles';
+
+function MovieDetails() {
+  const [data, setData] = useState({});
+  const params = useParams();
+  console.log(params);
+
+    async function getDetails() {
+      const response = await GetDetailsMovie(params.id);
+      setData(response);
+    }
+  
+  useEffect(() => {
+    getDetails();
+  },[]);
+
+  return (
+    <div className="conteudo-filme">
+      
+      <Card data={data} />
+    </div>
+  );
+}
+
+export default MovieDetails;
